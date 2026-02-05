fff9cb75bb02553b4f1cd8c77469c1d87323ff2a
Timur-ka01 <timur_a_y@mail.ru>
2026-02-06 00:21:57 +0500

    laba1/6

--- 1laba/06_songs_list.py
+++ 1laba/06_songs_list.py
@@ -23,8 +23,10 @@ violator_songs_list = [
 # Обратите внимание, что делать много вычислений внутри print() - плохой стиль.
 # Лучше заранее вычислить необходимое, а затем в print(xxx, yyy, zzz)
 
-# TODO здесь ваш код
-
+x = violator_songs_list[3][1]
+y = violator_songs_list[5][1]
+z = violator_songs_list[8][1]
+print('Три песни звучат', round(x + y + z, 2))
 # Есть словарь песен группы Depeche Mode
 violator_songs_dict = {
     'World in My Eyes': 4.76,
@@ -41,4 +43,7 @@ violator_songs_dict = {
 # распечатайте общее время звучания трех песен: 'Sweetest Perfection', 'Policy of Truth' и 'Blue Dress'
 #   А другие три песни звучат ХХХ минут
 
-# TODO здесь ваш код
+a = violator_songs_dict['Sweetest Perfection']
+b = violator_songs_dict['Policy of Truth']
+c = violator_songs_dict['Blue Dress']
+print('А другие три песни звучат', round(a + b + c, 0))
