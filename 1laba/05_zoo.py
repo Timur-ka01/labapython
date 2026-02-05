58dc134040366d8ba82db4582641bd8378a6062c
Timur-ka01 <timur_a_y@mail.ru>
2026-02-06 00:07:08 +0500

    laba1/5

--- 1laba/05_zoo.py
+++ 1laba/05_zoo.py
@@ -8,16 +8,29 @@ zoo = ['lion', 'kangaroo', 'elephant', 'monkey', ]
 # посадите медведя (bear) между львом и кенгуру
 #  и выведите список на консоль
 # TODO здесь ваш код
-
+zoo.append('')
+for i in range(1, 4):
+    zoo[-i] = zoo[-i - 1]
+    i -= 1
+zoo[1] = 'bear'
+print(zoo)
 # добавьте птиц из списка birds в последние клетки зоопарка
 birds = ['rooster', 'ostrich', 'lark', ]
 #  и выведите список на консоль
 # TODO здесь ваш код
+zoo.extend(birds[0:])
+print(zoo)
 
 # уберите слона
 #  и выведите список на консоль
 # TODO здесь ваш код
-
+zoo.pop(3)
+print(zoo)
 # выведите на консоль в какой клетке сидит лев (lion) и жаворонок (lark).
 # Номера при выводе должны быть понятны простому человеку, не программисту.
 # TODO здесь ваш код
+for i in range(0, 7):
+    if zoo[i] == 'lion' or zoo[i] == 'lark':
+        print(i + 1)
+    i + 1
+    
