80957accee092913ddfec6908cd7cbc900c07d9e
Timur-ka01 <timur_a_y@mail.ru>
2026-02-19 22:44:39 +0500

    05lab

--- 1laba/05_zoo.py
+++ 1laba/05_zoo.py
@@ -11,14 +11,14 @@ zoo = ['lion', 'kangaroo', 'elephant', 'monkey', ]
 zoo.append('')
 for i in range(1, 4):
     zoo[-i] = zoo[-i - 1]
-    i -= 1
+
 zoo[1] = 'bear'
 print(zoo)
 # добавьте птиц из списка birds в последние клетки зоопарка
 birds = ['rooster', 'ostrich', 'lark', ]
 #  и выведите список на консоль
 # TODO здесь ваш код
-zoo.extend(birds[0:])
+zoo.extend(birds)
 print(zoo)
 
 # уберите слона
@@ -32,5 +32,4 @@ print(zoo)
 for i in range(0, 7):
     if zoo[i] == 'lion' or zoo[i] == 'lark':
         print(i + 1)
-    i + 1
     
