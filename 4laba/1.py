2eda548a7c4c0207b3be64a0abc092931a536c55
Timur-ka01 <timur_a_y@mail.ru>
2026-03-22 14:26:24 +0500

    4laba

---
+++ 4laba/1.py
@@ -0,0 +1,29 @@
+import requests
+import time
+
+def pi():
+    def door():
+        r = requests.get('https://dogapi.dog/api/v2/facts')
+        data = r.json()
+        n = data['data'][0]['attributes']['body']
+        return n
+    return door()
+
+def limit_calls(x):
+    last_call = 0
+
+    def wrapper():
+        nonlocal last_call
+        now = time.time()
+
+        if now - last_call < 2:
+            return
+        last_call = now
+        return x()
+
+    return wrapper
+
+
+decorationpi = limit_calls(pi)
+print(decorationpi())
+
