368f9d729b9a1d459783ba30c6d21978e717db93
Timur-ka01 <timur_a_y@mail.ru>
2026-03-22 18:17:02 +0500

    .

--- 2laba/3.py
+++ 2laba/3.py
@@ -1,22 +1,16 @@
-def naiti(x):
-    p = 0
-    limit = int(x ** 0.5)
-    for i in range(1, limit + 1, 2):
-        if x % i == 0:
-            p += 1
-            para = x // i
-            if para != i and para % 2 != 0:
-                p += 1
-        if p > 5:
-            return False
-    if p == 5:
-        return x
-    else:
-        return False   
 def f():
-    for i in range(40000, 50001):
-        k = naiti(i)
-        if k:
-            print(k)
+    result = []
+    n = 452022
+    
+    while len(result) < 5:
+        for d in range(2, int(n**0.5) + 1):
+            if n % d == 0:
+                M = d + n // d
+                if M % 7 == 3:
+                    result.append((n, M))
+        n += 1
+        
+    return result
 
-f()
+
+print(f())
