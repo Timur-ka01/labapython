5c1c4c60c5f6371436d0f93dbb2feb200b661303
Timur-ka01 <timur_a_y@mail.ru>
2026-04-02 15:50:38 +0500

    Update 1.py

--- 4laba/1.py
+++ 4laba/1.py
@@ -1,19 +1,12 @@
 import requests
 import time
 
-def f(url):
-    @limit_calls
-    def f1():
-        r = requests.get(url)
-        data = r.json()
-        n = data['data'][0]['attributes']['body']
-        return n
-    return f1
+
 
 def limit_calls(x):
     last_call = 0
 
-    def wrapper():
+    def wrapper(*args, **kwargs):
         nonlocal last_call
         now = time.time()
 
@@ -21,17 +14,24 @@ def limit_calls(x):
             print('погоди')
             return None
         last_call = now
-        return x()
+        return x(*args, **kwargs)
 
     return wrapper
 
+@limit_calls
+def f(url):
+    def f1():
+        r = requests.get(url)
+        data = r.json()
+        n = data['data'][0]['attributes']['body']
+        return n
+    return f1
+
 
 p = f('https://dogapi.dog/api/v2/facts')
-decorationpi = limit_calls(p)
 
 print(p())
 time.sleep(1)
 print(p())
 time.sleep(3)
 print(p())
-
