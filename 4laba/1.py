0d10e19a345580e72da5573e4243c9976333564a
Timur-ka01 <timur_a_y@mail.ru>
2026-04-23 14:28:46 +0500

    4laba

--- 4laba/1.py
+++ 4laba/1.py
@@ -2,7 +2,7 @@ import requests
 import time
 from functools import wraps
 
-def limit_calls(x=None, delay=2):
+def limit_calls(delay=2):
     def decorator(func):
         last_call = 0
         
@@ -19,49 +19,15 @@ def limit_calls(x=None, delay=2):
             return func(*args, **kwargs)
         
         return wrapper
-    
-    if x is None:
-        return decorator
-    else:
-        return decorator(x)
+    return decorator
 
 
-@limit_calls
+@limit_calls()
 def get_fact(url):
     r = requests.get(url)
     data = r.json()
     n = data['data'][0]['attributes']['body']
     return n
-
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> 971e8e6f93bc0ccc3d2798cd0f5b2c5f3ce67675
-@limit_calls
-def f(url):
-    def f1():
-        r = requests.get(url)
-        data = r.json()
-        n = data['data'][0]['attributes']['body']
-        return n
-    return f1
-
-
-<<<<<<< HEAD
-=======
->>>>>>> 97cd910331d8b6215852ee3901dd11f4e1a050cf
->>>>>>> 971e8e6f93bc0ccc3d2798cd0f5b2c5f3ce67675
-p = f('https://dogapi.dog/api/v2/facts')
-=======
-url = 'https://dogapi.dog/api/v2/facts'
-print(get_fact(url))
-time.sleep(1)
-print(get_fact(url))
-time.sleep(3)
-print(get_fact(url))
 
 
 @limit_calls(delay=1)
@@ -71,7 +37,13 @@ def countdown(n):
     print(n)
     time.sleep(0.5)
     return countdown(n - 1)
->>>>>>> 4ac0866e370981904f7e3368d798315b48397d17
 
+
+url = 'https://dogapi.dog/api/v2/facts'
+print(get_fact(url))
+time.sleep(1)
+print(get_fact(url))
+time.sleep(3)
+print(get_fact(url))
 
 print(countdown(5))
